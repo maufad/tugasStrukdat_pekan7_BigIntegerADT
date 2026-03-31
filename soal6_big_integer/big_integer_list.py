7c445cbdf0dc46f4cdc3ad309de666fc957b0027
maufad <muhammadaufadhiyauddin@gmail.com>
2026-03-31 18:05:04 +0700

    Create big_integer_list.py

---
+++ soal6_big_integer/big_integer_list.py
@@ -0,0 +1,10 @@
+"""
+Big Integer ADT - Implementasi dengan Python List
+Digit disimpan dalam list, urutan LSB first (index 0 = satuan)
+"""
+
+class BigIntegerList:
+    """Big Integer ADT menggunakan Python List"""
+    
+    def __init__(self, init_value="0"):
+        """Membuat
